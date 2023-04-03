f3c9c07387559d222f97991eba4d5d1e1d43a5e7
first-react <13924840822@163.com>
2023-04-03 18:44:58 +0800

    home finished

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+
 
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+
+// 拦截器
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 响应器
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 Vue.config.productionTip = false
 
